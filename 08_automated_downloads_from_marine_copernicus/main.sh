0ed2e1a1cca8f9db5612f6a335b57851584ef774
Amaia <amaia.marcano@gmail.com>
2021-06-22 09:12:01 +0200

    ongoing

---
+++ 08_automated_downloads_from_marine_copernicus/main.sh
@@ -0,0 +1,4 @@
+mkdir data/output;
+
+# run the script
+env/bin/python ./src/send_the_jobs.py
---
+++ 08_automated_downloads_from_marine_copernicus/src/send_the_jobs.py
@@ -0,0 +1,5 @@
+# extract the files' paths
+
+
+# for each file in data/input:
+# send a job of that to the nodes
