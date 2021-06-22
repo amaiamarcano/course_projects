49c11051d672ef0499a70414bcec05b85b0c824d
Amaia <amaia.marcano@gmail.com>
2021-06-22 09:11:31 +0200

    finished

--- 07_imshow_and_quiver/src/script.py
+++ 07_imshow_and_quiver/src/script.py
@@ -33,13 +33,20 @@ file_path = "./data/input/cmems_ibi_example.nc"
 data = nc.Dataset(file_path, "r")
 
 time_values = data["time"]
+longitude_values = data["longitude"]
+latitude_values = data["latitude"]
 
 # for each time value (lines), for each variable : uo, vo, sqrt(uo^2+vo^2)
 # fig, axs = plt.subplots(len(time_values),3)
 
+# step
+s=10
+LON, LAT = np.meshgrid(longitude_values, latitude_values)
+
 # for each time value (3 of them)
 for i in range(len(time_values)):
 
+    fig, ax= plt.subplots()
     j = 0
     # plot plain uo and vo
     for var in ["uo","vo"]:
@@ -61,8 +68,26 @@ for i in range(len(time_values)):
     # axs[i,j].set_title(f"t={i};var=sqrt(uo^2+vo^2)")
     # axs[i,j].quiver(data.variables["uo"][i,0,:,:], data.variables["vo"][i,0,:,:])
 
-    plt.imshow(np.sqrt((data.variables["uo"][i,0,:,:])**2+(data.variables["vo"][i,0,:,:])**2))
-    plt.quiver(data.variables["uo"][i,0,:,:], data.variables["vo"][i,0,:,:])
-    plt.savefig(f"./data/output/plot_time_{i}.png", dpi=600)
 
-# plt.savefig("./data/output/plot.png", dpi=3000)
+    plt.imshow(
+        np.sqrt((data.variables["uo"][i,0,:,:])**2+(data.variables["vo"][i,0,:,:])**2),
+        cmap="rainbow",
+        interpolation="gaussian",
+        origin="lower",
+        extent=[min(longitude_values), max(longitude_values), min(latitude_values), max(latitude_values)],
+    )
+    plt.clim([0,0.2])
+    plt.colorbar()
+
+    plt.quiver(
+     LON[::s, ::s],
+     LAT[::s, ::s],
+     data.variables["uo"][i,0,::s,::s],
+     data.variables["vo"][i,0,::s,::s],
+     color="blue",
+     angles="xy",
+     scale_units="xy",
+     scale=1,
+    )
+    plt.savefig(f"./data/output/plot_time_{i}.png", dpi=600)
+    plt.close(fig)
